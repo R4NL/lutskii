0d13ff77b5c8aa55119fb1c9b3de9de7f4e2e604
Thealeshka <alexfenin1234@gmail.com>
2019-09-11 15:06:27 +0300

    add static method and clusterforlab2

--- src/main/java/com/Thealeshka/data/cluster/ClusterForLab1.java
+++ src/main/java/com/Thealeshka/data/cluster/ClusterForLab1.java
@@ -5,6 +5,8 @@ import com.Thealeshka.data.cluster.vertex.comparators.VertexComparator;
 
 import java.util.*;
 
+import static com.Thealeshka.data.cluster.ClusterForLab2.getList;
+
 public class ClusterForLab1 {
     private List<Vertex> list;
 
@@ -14,16 +16,10 @@ public class ClusterForLab1 {
     public List<Vertex> createCluster(int startPoint) {
         list = new ArrayList<>();
         for (int i = 1; i <= 9; i++) {
-            list.add(new Vertex(i+startPoint*9));
+            list.add(new Vertex(i + startPoint * 9));
         }
         int[][] matrix = {{2, 4, 5}, {1, 3}, {2, 5, 6}, {1, 7}, {1, 3, 7, 9}, {3, 9}, {4, 5, 7}, {7, 9}, {5, 6, 8}};
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                list.get(i).setNeighboringVertex(list.get(matrix[i][j] - 1).getId());
-            }
-        }
-        Collections.sort(list, new VertexComparator());
-        return list;
+        return getList(matrix, list);
     }
 }
